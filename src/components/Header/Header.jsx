7ff34d706ef7a0b6f030decdbf5651c54dc712c4
Ravi <ravikumarjha059@gmail.com>
2023-08-07 22:03:46 +0530

    left-part completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,28 @@
 import "./Header.scss";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { AiFillGithub } from "react-icons/ai";
+import { BsBook } from "react-icons/bs";
+import { RiGitRepositoryLine } from "react-icons/ri";
 const Header = () => {
   return (
     <div className="header">
+      {/* Header Left Part */}
       <div className="header_Left">
         <div className="header_LeftUpper">
           <RxHamburgerMenu />
           <AiFillGithub />
-          <h2>vr1Ravi</h2>
+          <p>vr1Ravi</p>
         </div>
-        <div className="header_LeftLower"></div>
+        <div className="header_LeftLower">
+          <div className="overview">
+            <BsBook /> <p>Overview</p>
+          </div>
+          <div className="repos">
+            <RiGitRepositoryLine /> <p>Repositories</p> <span>12</span>
+          </div>
+        </div>
       </div>
+      {/* Header Right Part */}
       <div className="header_Right"></div>
     </div>
   );
