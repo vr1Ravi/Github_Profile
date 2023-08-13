2b659f6239c6b42515a9bfba54d08364ef9d5858
Ravi <ravikumarjha059@gmail.com>
2023-08-14 03:30:35 +0530

    searchbard added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
+import { Provider } from "react-redux";
+import store from "./store";
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Header />} />
-        </Routes>
+        <Provider store={store}>
+          <Routes>
+            <Route path="/" element={<Header />} />
+          </Routes>
+        </Provider>
       </BrowserRouter>
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,30 @@ import { RxHamburgerMenu } from "react-icons/rx";
 import { AiFillGithub } from "react-icons/ai";
 import { BsBook } from "react-icons/bs";
 import { RiGitRepositoryLine } from "react-icons/ri";
+import { BiSearchAlt2 } from "react-icons/bi";
+import { useState, useEffect } from "react";
 const Header = () => {
+  const [inputVal, setInputVal] = useState("");
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const handleChange = (e) => {
+    // setInputVal(e.target.value);
+    const name = e.target.value;
+    setInputVal(name);
+  };
+  useEffect(() => {
+    if (inputVal === "") return;
+    console.log(inputVal);
+    const timer = setTimeout(async () => {
+      const res = await fetch(`https://api.github.com/users/${inputVal}`);
+      const data = await res.json();
+      console.log(data);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [inputVal]);
+  window.addEventListener("resize", () => {
+    setWindowWidth(window.innerWidth);
+  });
   return (
     <div className="header">
       {/* Header Left Part */}
@@ -23,7 +46,30 @@ const Header = () => {
         </div>
       </div>
       {/* Header Right Part */}
-      <div className="header_Right"></div>
+      <div className="header_Right">
+        {windowWidth > 900 ? (
+          <>
+            <BiSearchAlt2 />
+            <input
+              type="text"
+              // value={inputVal}
+              onChange={(e) => handleChange(e)}
+            />
+          </>
+        ) : (
+          <div className="inutBox">
+            {" "}
+            <BiSearchAlt2 />
+          </div>
+        )}
+
+        <div className="prifile_img">
+          <img
+            src="https://avatars.githubusercontent.com/u/103517405?v=4"
+            alt=""
+          />
+        </div>
+      </div>
     </div>
   );
 };
