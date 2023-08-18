fbeb1773f89ab5fcdaf976d894ad61acd3f447fc
Ravi <ravikumarjha059@gmail.com>
2023-08-18 16:17:26 +0530

    Blank

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,17 +1,37 @@
 // import Skeleton from "react-loading-skeleton";
 import "./Hero.scss";
 import { useSelector } from "react-redux";
+import { HiOutlineLocationMarker } from "react-icons/hi";
 const Hero = () => {
   const user = useSelector((state) => state.user.value);
   return (
     <div className="hero">
       <div className="leftHero">
-        <img src={user?.avatar_url} alt="img" />
-        <div className="name">
-          <h1>Ravishankar</h1>
-          <p>vr1Ravi.</p>
+        <img
+          src={
+            user
+              ? user.avatar_url
+              : "https://avatars.githubusercontent.com/u/103517405?v=4"
+          }
+          alt="img"
+        />
+        <div className="details">
+          <h1>{user ? user.name : "userName.."}</h1>
+          <p>{user ? user.login : "login.."}</p>
+          <p>{user ? user.bio : "search user to get thier bio"}</p>
         </div>
-        <div className="about"></div>
+        <div className="social">
+          {" "}
+          <p>
+            <span>1</span> follower . <span>0</span> following
+          </p>
+        </div>
+        <div className="address">
+          <address>
+            <HiOutlineLocationMarker /> A-2202, Gardenia Square, Crossing
+            Republik, Ghaziabad
+          </address>
+        </div>
       </div>
       <div className="rightHero"></div>
     </div>
